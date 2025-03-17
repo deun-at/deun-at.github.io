1a7624d6ff318cfb4b54d8cc660f4af2d9cb0ee8
thejagger <jackpot.auer5@gmail.com>
2025-03-17 23:07:28 +0000

    Deploying to main from @ deun-at/deun@47cf4ec2afe52aefe4bd4821459a3e49b5b28559 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3303438963"
+    serviceWorkerVersion: "660643399"
   }
 });
