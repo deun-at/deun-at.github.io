1272ead70ef91847afac0dbd49eb18adb13a8cff
thejagger <jackpot.auer5@gmail.com>
2026-08-15 10:10:27 +0000

    Deploying to main from @ deun-at/deun@841ae98116031bf3889bfe6740c0d00c5af2963d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1279267275" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2244949964" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
